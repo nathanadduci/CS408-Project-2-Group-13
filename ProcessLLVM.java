3b3764991a184fe67db10316b16ce56df59a6d0a
Nathan Adduci <nathanadduci@gmail.com>
2020-03-04 22:27:44 -0500

    Coverage Graph of the data.

--- ProcessLLVM.java
+++ ProcessLLVM.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,12 +18,26 @@ public class ProcessLLVM {
 
         HashMap<String, Integer> covg = getCoverage(graph);
 
-        for (HashMap.Entry<String, Integer> entry : covg.entrySet()) {
+        /**
+        //Printing to debug graph coverage.
+         for (HashMap.Entry<String, Integer> entry : covg.entrySet()) {
             String key = entry.getKey();
             Integer val = entry.getValue();
             System.out.println(key + ": " + val);
-        }
+        }//*/
 
+        HashMap<String, HashMap<String, Integer>> covp = getPairCoverage(graph);
+
+        //**
+        //Printing to debug graph coverage.
+        for (HashMap.Entry<String, HashMap<String, Integer>> entry : covp.entrySet()) {
+            String key = entry.getKey();
+            HashMap<String, Integer> map2 = entry.getValue();
+            for(HashMap.Entry<String, Integer> entry2 : map2.entrySet()){
+                System.out.println("("+key+", "+entry2.getKey()+"): "+entry2.getValue());
+            }
+            // System.out.println("("+key.getKey() + ", " + key.getValue() + "): " + val);
+        }//*/
 
         /**
         // Basic printing for testing graph completion / correctness.
@@ -38,7 +54,7 @@ public class ProcessLLVM {
                 }
             }
             System.out.println("}");
-        }// */
+        }//*/
     }
 
     public static HashMap<String, Integer> getCoverage(HashMap<String, HashSet<String>> graph) {
@@ -57,9 +73,61 @@ public class ProcessLLVM {
         return covg;
     }
 
-    public static HashMap<Map.Entry<String, String>, Integer> getPairCoverage(HashMap<String, HashSet<String>> graph) {
+    public static HashMap<String, HashMap<String, Integer>> getPairCoverage(HashMap<String, HashSet<String>> graph) {
+        HashMap<String, HashMap<String, Integer>> covp = new HashMap<String, HashMap<String, Integer>>();
+        for(HashMap.Entry<String, HashSet<String>> entry : graph.entrySet()){
+            //String key = entry.getKey();
+            String vals[] = new String[entry.getValue().size()];
+            vals = entry.getValue().toArray(vals);
+            for (int i = 0; i < vals.length; i++) {
+                for(int j = i + 1; j < vals.length; j++){
+                    //Pair<String, String> key = getPair(vals[i],vals[j], 0);
+                    if(covp.get(vals[i]) == null) {
+                        HashMap<String, Integer> tmp = new HashMap<>();
+                        tmp.put(vals[j], 1);
+                        covp.put(vals[i], tmp);
+                    } else if(covp.get(vals[i]).get(vals[j]) == null) {
+                        covp.get(vals[i]).put(vals[j], 1);
+                    } else {
+                        Integer val = covp.get(vals[i]).get(vals[j]);
+                        covp.get(vals[i]).replace(vals[j], val+1);
+                    }
+                    if(covp.get(vals[j]) == null) {
+                        HashMap<String, Integer> tmp = new  HashMap<>();
+                        tmp.put(vals[i], 1);
+                        covp.put(vals[i], tmp);
+                    } else if(covp.get(vals[j]).get(vals[i]) == null) {
+                        covp.get(vals[j]).put(vals[i], 1);
+                    } else {
+                        Integer val = covp.get(vals[j]).get(vals[i]);
+                        covp.get(vals[j]).replace(vals[i], val+1);
+                    }
+                }
+            }
+        }
+        return covp;
+    }
 
-        return null;
+    private static Pair<String, String> getPair(String si, String sj, int i){
+        if(si.length() > i || sj.length() > i){
+            if(si.length() > sj.length()){
+                return new Pair<String, String>(sj, si);
+            } else if(si.length() < sj.length()) {
+                return new Pair<String, String>(si, sj);
+            } else {
+                if(si.charAt(i) < sj.charAt(i)){
+                    return new Pair<String, String>(si, sj);
+                } else {
+                    return new Pair<String, String>(sj, si);
+                }
+            }
+        } else if(si.charAt(i) < sj.charAt(i)) {
+            return new Pair<String, String>(si, sj);
+        } else if(si.charAt(i) > sj.charAt(i)) {
+            return new Pair<String, String>(sj, si);
+        } else {
+            return getPair(si, sj, i+1);
+        }
     }
 
     public static HashMap<String, HashSet<String>> getGraph(){
